db9b3250a47e8a31d3be63d739fca9f298785320
Christopher P. Calub <chrispcalub@gmail.com>
2015-10-07 23:08:40 +0800

    Create register.php

---
+++ register.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html>
+<head>	
+<title>Register</title>
+</head>
+<body>
+
+<h3>Registration Form</h3>
+<form action="" method="GET">
+Username: <input type=text name='user'><br/>
+Password: <input type=password name='pass'><br/>	
+
+<input type=submit value='Register' name='submit'>
+</form>
+<?php 
+if (isset($_GET['submit'])){
+
+if(!empty($_GET['user']) && !empty($_['pass'])) {
+
+//mysql_real_escape_string() escapes special characters in a string for use in an SQL statement
+$user=mysql_real_escape_string($_POST['user']); 
+$pass=mysql_real_escape_string($_POST['pass']);
+
+$con=mysql_connect('localhost','root','') or die(mysql_error());
+mysql_select_db('user') or die("cannot select DB");
+
+$query=mysql_query("SELECT * FROM login WHERE user='".$user."'");
+$numrows=mysql_num_rows($query);
+if($numrows==0)
+{
+//md5() calculates the MD5 hash of a string
+$encrypt_password=md5($pass);
+
+$sql="INSERT INTO login(user,pass) VALUES('$user','$encrypt_password')";
+
+$result=mysql_query($sql);
+
+
+if($result!=1) 
+{
+echo "Failure!";
+}
+else{
+echo "Account Successfully Created";
+}
+} else {
+echo "That username already exists! Please try again with another.";
+}
+
+} else {
+	echo "All fields are required!";
+}
+}
+?>
+<p><a href="register.php">Register</a> | <a href="login.php">Login</a></p>
+</body>
+</html>
